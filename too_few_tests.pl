4e0a7ddc43a475f48ff767cd8200f4384dd3ee0e
Lutz Filor <lutz@pacbell.net>
2021-04-10 12:50:40 -0700

    Add *.pl files to show the difference between          *.t and *.pm, *pl files in testing

---
+++ too_few_tests.pl
@@ -0,0 +1,6 @@
+use Test::More tests => 3;
+
+pass( 'test_one' );
+pass( 'test_two' );
+pass( 'test_three' );
+
---
+++ too_many_tests.pl
@@ -0,0 +1,6 @@
+use Test::More tests => 2;
+
+pass( 'test_one' );
+pass( 'test_two' );
+pass( 'test_three' );
+
